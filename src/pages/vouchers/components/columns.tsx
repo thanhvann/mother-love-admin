b13e1766031ae4a4b4324b6303ce44fa063d7185
thanhvann <tvtvan1407@gmail.com>
2024-07-11 21:53:45 +0700

    fix status voucher

--- src/pages/vouchers/components/columns.tsx
+++ src/pages/vouchers/components/columns.tsx
@@ -5,12 +5,11 @@ import { Button } from "@/components/ui/button";
 
 import { Icons } from "@/components/ui/icons";
 
-
 import { VoucherObj } from "@/models/Voucher";
 import CellAction from "./cell-action";
+import { VoucherStatuses } from "@/components/DataTable/filters";
 
 export const columns: ColumnDef<VoucherObj>[] = [
-
   {
     accessorKey: "voucherCode",
     header: ({ column }) => (
@@ -47,9 +46,34 @@ export const columns: ColumnDef<VoucherObj>[] = [
     accessorKey: "endDate",
     header: "End Date",
   },
+  // {
+  //   accessorKey: "status",
+  //   header: "Status",
+  // },
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = VoucherStatuses.find(
+        (status) => status.value === row.getValue("status")
+      );
+
+      if (!status) {
+        return null;
+      }
+
+      return (
+        <div className="flex w-[100px] items-center">
+          {status.icon && (
+            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+          )}
+          <span>{status.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
   },
   {
     id: "actions",
